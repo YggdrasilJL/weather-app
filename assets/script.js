843ea0219b8943e66d153405f0b37d18b49d311c
YggdrasilJL <yggdrasiljl@gmail.com>
2023-06-03 19:19:30 -0300

    add api, reset css and start index

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+const apiKey = 'b329df0af2f27dbc7b382270d5a27148'
+const lat = '45.907990'
+const long = '-66.530410'
+const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`

99ccfd485e4fb316226ba5e7e4537361fa335dcc
YggdrasilJL <yggdrasiljl@gmail.com>
2023-06-19 14:32:36 -0300

    bootstrap is annoying

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,27 @@
 const apiKey = 'b329df0af2f27dbc7b382270d5a27148'
-const lat = '45.907990'
-const long = '-66.530410'
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`
+//let lat;
+//let long;
+let combinedCord;
+const cityName = 'Halifax'
+const apiCity = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`
+//const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`
+
+fetch(apiCity).then(function (data) {
+    return data.json()
+}) .then (function (data) {
+    console.log(data)
+    console.log(data[0].lat)
+    console.log(data[0].lon)
+    let lat = data[0].lat
+    let long = data[0].lon
+    getWeather(lat,long)
+}) 
+function getWeather(lat, long){
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`
+fetch(apiUrl).then(function (data) {
+     return data.json()
+     }) .then(function (data) {
+     console.log(data)
+     console.log(data.list[1].main.humidity)
+ })
+}
